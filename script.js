70b71c120b5f7d9c4842fb8dabfa825ea13b6fa8
divya7349 <divyabalaji2017@gmail.com>
2020-07-25 12:17:34 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const subHeader=document.querySelector(".sub-header");
+const collapasebleToggle=document.querySelector(".collapaseble-menu");
+let slideIndex=0;
+const links= document.querySelectorAll('.link');
+
+function removeLinks(){
+    for(let i=0;i<links.length;i++){
+       links[i].addEventListener('click', ()=>{
+           subHeader.classList.toggle('active');
+           collapasebleToggle.classList.toggle('active');
+       })
+    }
+}
+collapasebleToggle.addEventListener('click', (e)=>{
+
+    collapasebleToggle.classList.toggle('active');
+    subHeader.classList.toggle('active');
+});
+
+window.addEventListener('scroll', ()=>{
+   
+    const scroll=document.documentElement.scrollTop
+    
+    if(scroll > 0){
+        subHeader.classList.add('fixed');
+    }else{
+        subHeader.classList.remove('fixed');
+    }
+
+    
+});
+
+function carousel(){
+    let i;
+    let x= document.getElementsByClassName("mySlides");
+    for(i=0;i<x.length;i++){
+       x[i].style.display="none";
+
+    }
+    slideIndex++;
+    if(slideIndex>x.length){slideIndex=1}
+    x[slideIndex-1].style.display="block";
+    setTimeout(carousel,2000);
+}
+carousel();
+removeLinks();
